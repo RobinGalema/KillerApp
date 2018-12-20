874e5c5400ecaa232dc841d6ab83d562a476cefb
Robin Galema <43341146+RobinGalema@users.noreply.github.com>
2018-12-20 09:58:52 +0100

    Added Database entities

--- Killer App/KillerApp/KillerApp/Game.cs
+++ Killer App/KillerApp/KillerApp/Game.cs
@@ -8,6 +8,28 @@ namespace KillerApp
 {
     class Game
     {
+        // Fields
+        private string name;
+        private decimal price;
+        private Developper developper;
+        private Genre genre;
+        private List<players> players = new List<players>();
 
+        //Constructor
+        public Game(string name, decimal price, Developper developper, Genre genre, List<players> players)
+        {
+            this.name = name;
+            this.price = price;
+            this.developper = developper;
+            this.genre = genre;
+            this.players = players;
+        }
+
+        // Properties
+        public string Name { get => name; private set => name = value; }
+        public decimal Price { get => price; private set => price = value; }
+        public Developper Developper { get => developper; private set => developper = value; }
+        public Genre Genre { get => genre; private set => genre = value; }
+        public List<players> Players { get => players; private set => players = value; }
     }
 }
