08a028ba261699e4499c185c910df4c7dba88442
Robin Galema <43341146+RobinGalema@users.noreply.github.com>
2018-12-20 10:15:02 +0100

    Added connection string for database

--- Killer App/KillerApp/KillerApp/Form1.cs
+++ Killer App/KillerApp/KillerApp/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace KillerApp
 {
     public partial class Form1 : Form
     {
+        const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= C:\Users\Robin\OneDrive\Documents\GitHub\KillerApp\Killer App\KillerApp\KillerApp\DBapp.mdf ;Integrated Security = True";
+        SqlConnection conn = new SqlConnection(connectionString);
+
         public Form1()
         {
             InitializeComponent();
